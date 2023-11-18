2c23d6faf58b447a3e9dd3c8a242b46f19504f25
charmainemimie <82668352+charmainemimie@users.noreply.github.com>
2023-11-18 20:31:02 +0100

    finished setting up vite and chakra

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Navbar from './Components/Navbar'
+function App() {
+//const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+      <Navbar/>
+      </div>
+  
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,9 @@
+function Navbar() {
+  return (
+    <div>
+        <p>This is my Navbar</p>
+    </div>
+  )
+}
+
+export default Navbar
